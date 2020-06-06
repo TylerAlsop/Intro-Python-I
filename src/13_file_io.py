5d31ec76a2892f29038e48beb020cff8cb0808b7
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-05 19:06:41 -0600

    Finished 13. The final task in 13 created the bar.txt fil.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,23 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('src/foo.txt') as foo_file:
+    print(foo_file.read())
+foo_file.close()
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("src/bar.txt", "w") as bar_file:
+    bar_file.write("This is line 1.\n")
+    bar_file.write("This is line 2.\n")
+    bar_file.write("This is line 3.")
+
+with open("src/bar.txt") as bar_file:
+    print(bar_file.read())
+bar_file.close()
